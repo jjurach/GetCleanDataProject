970b850084bd4f7c2c9a4bc874130e68c588ea7c
James Jurach <james.jurach@gmail.com>
2015-04-26 02:16:12 -0500

    rename keep_columns

--- run_analysis.R
+++ run_analysis.R
@@ -9,13 +9,13 @@ run_analysis <- function(dir="UCI HAR Dataset") {
   activity_labels <- read.table(file.path(dir, "activity_labels.txt"))[,2]
   features <- read.table(file.path(dir, "features.txt"), col.names=c('n','name'))[,2]
   
-  keep_columns = features[grep("-(mad|max|min|sma|energy|iqr|entropy|maxInds|meanFreq|skewness|kurtosis)..$", features, invert=TRUE)]
+  keep_features = features[grep("-(mad|max|min|sma|energy|iqr|entropy|maxInds|meanFreq|skewness|kurtosis)..$", features, invert=TRUE)]
   
   case <- "test"
   if (is.null(X_TEST)) {
     X_TEST  <- read.table(file.path(dir, case, "X_test.txt"), col.names=features)
   }
-  X_TEST <- X_TEST[, keep_columns]
+  X_TEST <- X_TEST[, keep_features]
   more <- data.frame(
     subject=read.table(file.path(dir, case, "subject_test.txt"))[,1],
     activity=sapply(read.table(file.path(dir, case, "y_test.txt"))[,1], function(x) activity_labels[x]),
@@ -27,7 +27,7 @@ run_analysis <- function(dir="UCI HAR Dataset") {
   if (is.null(X_TRAIN)) {
     X_TRAIN <- read.table(file.path(dir, case, "X_train.txt"), col.names=features)
   }
-  X_TRAIN <- X_TRAIN[, keep_columns]
+  X_TRAIN <- X_TRAIN[, keep_features]
   more <- data.frame(
     subject=read.table(file.path(dir, case, "subject_train.txt"))[,1],
     activity=sapply(read.table(file.path(dir, case, "y_train.txt"))[,1], function(x) activity_labels[x]),
